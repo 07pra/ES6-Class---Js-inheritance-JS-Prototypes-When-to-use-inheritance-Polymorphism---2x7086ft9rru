e504be953af1057ecae9e064474d60d641eecdac
Prashant kumar <120910332+07pra@users.noreply.github.com>
2023-05-27 14:22:27 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
-//Write your code here
+class API{
+    #secure;
+    url;
+    method;
+    constructor(url){
+        this.url=url;
+        if(url.includes("https"))
+        this.#secure=true;
+        else
+        this.#secure=false;
+        this.method="GET"
+    }
+    isSecure(){
+        return this.#secure;
+    }
+    updateUrl(url){
+        this.url=url
+        if(url.includes("https"))
+        this.#secure=true;
+        else
+        this.#secure=false;
+    }
+}
 
+module.exports = { API }
 
-
-
-
-//Do not remove this code
-module.exports = { API }
+ES6 Class
